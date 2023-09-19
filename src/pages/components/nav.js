19c2bec9842457a3d40b31aed82a91faf25fba37
rahathosen <rahathosen.me@gmail.com>
2023-09-19 20:50:53 +0600

    up

--- src/pages/components/nav.js
+++ src/pages/components/nav.js
@@ -22,6 +22,7 @@ import {
   SquaresPlusIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
+import CurrentTIme from "./currentTime";
 
 const products = [
   {
@@ -75,12 +76,7 @@ export default function Nav() {
               <div className=" hidden relative z-10 lg:flex px-2 lg:px-0">
                 <div className="flex flex-shrink-0 items-center">
                   <div className="text-sm tracking-tight">
-                  <span className="bg-gradient-to-br from-gray-900 via-gray-800 to-gray-400  text-sm bg-clip-text text-center font-heading  tracking-[-0.02em] text-transparent drop-shadow-sm duration-300 ease-linear animate-in zoom-in-50 dark:bg-gradient-to-br dark:from-gray-100 dark:to-gray-900  md:leading-[5rem]">
-                  <span className="font-semibold ">Wednesday</span>, September
-                    13, 2023
-          </span>
-                    {/* <span className="font-semibold ">Wednesday</span>, September
-                    13, 2023 */}
+                    <CurrentTIme/>
                   </div>
                 </div>
               </div>
